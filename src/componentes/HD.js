3bf64708f946d65a6748b294770ef8b1c7de6e70
P4TO91 <diego.rueda91@gmail.com>
2024-03-02 10:40:42 -0500

    primer codigo en React Js

---
+++ src/componentes/HD.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// Header.jsx
+
+import React from 'react';
+
+const HD = () => {
+  return (
+    <header>
+      <img className="left-image" src="/Users/diegorueda/Documents/Estudios/Sena/Carrera Tecnologica SENA/ANALISIS/Analisis Proyecto Rama Judicial/Escudo_de_Colombia-logo-3544F5549B-seeklogo.com.png" alt="escudo de Colombia" />
+      <nav className="nav-rectangulo">
+        <ul>
+          <li><a href="#">Quiénes somos</a></li>
+          <li><a href="#">Ayuda</a></li>
+          <li><a href="#">Dudas e inquietudes</a></li>
+        </ul>
+      </nav>
+      <img className="right-image" src="/Users/diegorueda/Documents/Estudios/Sena/Carrera Tecnologica SENA/ANALISIS/Analisis Proyecto Rama Judicial/CSJ-01.png" alt="Logo Consejo Superior de Judicatura" />
+    </header>
+  );
+};
+
+export default HD;
